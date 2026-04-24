2ce91d4abc07ce5cc68a272010d012e0c4f006c8
Sophia <193290223+soap-phia@users.noreply.github.com>
2026-04-24 12:18:27 -0400

    a

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -9,16 +9,6 @@ export default function Layout({ children }: { children: JSX.Element }) {
 			<Helmet>
 				<title>{profile.name}</title>
 				<link rel="icon" href={profile.avatar} />
-				<meta content=":3" name="description" />
-				<meta content={profile.name} property="og:title" />
-				<meta content=":3" property="og:description" />
-				<meta content={profile.avatar} property="og:image" />
-				<meta content={profile.name} property="twitter:title" />
-				<meta content=":3" property="twitter:description" />
-				<meta content={profile.avatar} property="twitter:image" />
-				<meta property="og:type" content="website" />
-				<meta content="summary_large_image" name="twitter:card" />
-				<meta content="width=device-width, initial-scale=1" name="viewport" />
 			</Helmet>
 			<Navbar />
 			{children}
